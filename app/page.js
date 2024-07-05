211c8962022898c7a382571bf6ec910b2112f53d
Keshav Goyal <120664258+Keshav-Goyal-04@users.noreply.github.com>
2024-07-05 16:41:16 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h2>Aryan Munjral</h2>
+      <h2>Keshav Goyal</h2>
       <Button>Subscribe</Button>
     </div>
   );
